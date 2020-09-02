a18c51ccc26deeb45f595ed2dfcca47c09ee8743
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-02 23:17:56 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,11 @@ def my_each(words)
   i = 0
 while words.index < i
   yield(words)
+  i += 1
 end
 end
 
-
+my_each
 
 #each takes an array as an argument, and iterates over every element of it
 #it stops when it reaches the end of the array, and begins at the top of the array
