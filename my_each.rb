e52c1b2c38836ef8cd069824f4794a2d45be10ec
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-02 23:04:11 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,12 @@
 def my_each(argument)
-while argument.index < i do
+while argument.index < i
+  for argue in argument
   yield(argument)
   print i
 end
 end
+
+
+
+#each takes an array as an argument, and iterates over every element of it
+#it stops when it reaches the end of the array, and begins at the top of the array
