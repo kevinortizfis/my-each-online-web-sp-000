2f3c34b3eb4baf24a59ea9282bb4394fe5cdee04
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-02 23:17:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -10,15 +10,3 @@ end
 
 #each takes an array as an argument, and iterates over every element of it
 #it stops when it reaches the end of the array, and begins at the top of the array
-
-
-def writing(hand)
-  fingers = 0
-  while fingers > 6
-  "left"
-  fingers =+ 1
-end
-if fingers < 5
- "blue"
-end
-end
