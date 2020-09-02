63d46835abf964769e84dc053020f194d5d07e2a
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-02 23:18:09 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -6,7 +6,6 @@ while words.index < i
 end
 end
 
-my_each(words)
 
 #each takes an array as an argument, and iterates over every element of it
 #it stops when it reaches the end of the array, and begins at the top of the array
